a3a4dbc95756e03dcbec50d6d75b2ecff334d57e
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-04-16 09:46:32 +0500

    new js lab

---
+++ Section I/JavaScript/first_lab_of_js.js
@@ -0,0 +1,39 @@
+// console.log("Hello World");
+// const a = {a:10, b:20, c:30};
+// console.log (typeof(a))
+
+
+
+
+// var a= "10"
+// a+="20"
+// console.log(a)
+
+var a = -10
+
+// a > 0 ? console.log("Positive") : a< 0 ? console.log("Negative") : console.log("Zero") 
+
+// if (a > 0)
+// {
+//     console.log("Positive")
+// }
+// else if (a < 0)
+// {
+//     console.log("Negative")
+// }
+// else
+// {
+//     console.log("Zero")
+// }
+
+switch(true)
+{
+    case a > 0:
+        console.log("Positive")
+        break
+    case a < 0:
+        console.log("Negative")
+        break
+    default:
+        console.log("Zero")
+}
