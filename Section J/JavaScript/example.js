8bd928e044b945f7ea32cd90eb7ec9ec51ec70cf
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-05-09 08:37:36 +0500

    new

---
+++ Section J/JavaScript/example.js
@@ -0,0 +1,39 @@
+// function addTwoNums(a, b) {
+//   sum = a + b;
+//   return sum;
+// }
+// var sum = addTwoNums(5, 3);
+// console.log(sum);
+
+// var grains = ["wheat", "rice", "corn", "barley", "oats"];
+
+// // grains.push('quinoa');
+// // grains.push('millet');
+
+// // for (var i = 0; i < grains.length; i++) {
+// //   console.log(grains[i]);
+// // }
+
+// function displayGrains(grains) {
+//   for (var i = 0; i < grains.length; i++) {
+//     console.log(grains[i]);
+//   }
+// }
+
+// displayGrains(grains);
+// var arrayOfKeys = ["legs", "color", "material", "price"];
+// var table = {
+//   legs: 4,
+//   color: "brown",
+//   material: "wood",
+//   price: 100,
+// };
+// for (var i = 0; i < arrayOfKeys.length; i++) {
+//   console.log(table[arrayOfKeys[i]]);
+// }
+
+
+
+// console.log(table);
+// console.log(table.type);
+// console.log(table["color"]);
