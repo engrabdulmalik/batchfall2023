b9ce7cd7929a8001c8d71cf2f902daf7d02020b0
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-05-14 10:20:22 +0500

    new

---
+++ Section I/JavaScript/objects.js
@@ -0,0 +1,21 @@
+// console.log(Math.random() * 1000);
+
+// var myValue = "lorem, ipsum, ho, ho, ho, ha, ham, ha,"
+
+// // console.log(myValue.indexOf("ho") )// 10
+// // console.log(myValue.lastIndexOf("ho")) // 14
+
+// console.log(myValue.split(", "))
+
+var car = {}
+
+car.mileage = 98888
+car.color= "red"
+console.log(car)
+
+car.turnTheKey = function (){
+    console.log("Vroom Vroom")
+}
+
+car.turnTheKey()
+
