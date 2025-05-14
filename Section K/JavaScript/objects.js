64a35e912c5b19e868440a8cb3ce4c4440638573
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-05-14 15:41:28 +0500

    new

---
+++ Section K/JavaScript/objects.js
@@ -0,0 +1,11 @@
+// console.log(Math.PI)
+
+// console.log(Math.random())
+
+
+var myString = "Hello World lorem ipsum"
+
+// console.log(myString.indexOf(' '))
+// console.log(myString.lastIndexOf('l')) // 11
+
+console.log(myString.split(' '))
