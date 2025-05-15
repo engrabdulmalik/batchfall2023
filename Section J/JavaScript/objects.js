6eb5f917b7bcc194fbb231a6e2bb46c1e8d7b67b
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-05-15 15:13:26 +0500

    new

---
+++ Section J/JavaScript/objects.js
@@ -0,0 +1,19 @@
+// console.log(Math.random() ) //Math.random()
+
+
+// console.log("HELLO".toLowerCase()) //Math.random()
+
+
+var car ={}
+car.make = "Toyota"
+car.model = "Corolla"
+car.year = 2020
+car.color = "red"
+car.start = function() {
+    console.log("Car started")
+}
+car.stop = function() {
+    console.log("Car stopped")
+}
+
+car.start()//Toyota
