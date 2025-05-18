3020e5a22152533250a6b2ce2dda0a6a72082df1
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-05-18 14:12:31 +0500

    new

---
+++ Section J/JavaScript/errors.js
@@ -0,0 +1,15 @@
+
+try {
+    var c = 5;
+    var d = 0;
+    console.log(c/d)
+}
+catch (error) {
+    console.log("Error: Please don't input 0 as denominator " + error);
+    }
+
+
+
+var a = 5;
+var b = 10;
+console.log(a + b);
